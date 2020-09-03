deadebd1f15219e1a611647f92b8dbedf7e1aa44
Fredrik Makila <fredrik@takanoha.io>
2020-09-03 23:03:41 +0300

    Add e2e test for entry point

--- index.ts
+++ index.ts
@@ -1,3 +1,43 @@
 import Parser from './src/Parser';
+import {
+  Roster,
+  Cost,
+  CostType,
+  Force,
+  Category,
+  Selection,
+  Rule,
+  Profile,
+  TypeName,
+  AbilityProfile,
+  UnitProfile,
+  WeaponProfile,
+  WoundTrackProfile,
+  TransportProfile,
+  PsychicPowerProfile,
+  PsykerProfile,
+  ExplosionProfile,
+  UnknownProfile
+} from './src/types';
 
-export { Parser as default };
+export {
+  Parser as default,
+  Roster,
+  Cost,
+  CostType,
+  Force,
+  Category,
+  Selection,
+  Rule,
+  Profile,
+  TypeName,
+  AbilityProfile,
+  UnitProfile,
+  WeaponProfile,
+  WoundTrackProfile,
+  TransportProfile,
+  PsychicPowerProfile,
+  PsykerProfile,
+  ExplosionProfile,
+  UnknownProfile
+};
